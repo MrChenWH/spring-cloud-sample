81b6eba4e78a3fa704551fe74b3bf9ee4d1aabda
cwh <805911956qq@gmail.com>
2017-12-22 17:46:46 +0800

    修改参数

--- discovery-consul/src/main/java/com/example/cwh/DiscoveryConsulApplication.java
+++ discovery-consul/src/main/java/com/example/cwh/DiscoveryConsulApplication.java
@@ -1,21 +1,36 @@
 package com.example.cwh;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 注册中信Consul
  *
  * @author cwh
  */
+@SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-@SpringBootApplication
 public class DiscoveryConsulApplication
 {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    public String serviceUrl() {
+        List<ServiceInstance> list = discoveryClient.getInstances("STORES");
+        if (list != null && list.size() > 0 ) {
+            return list.get(0).getUri().toString();
+        }
+        return null;
+    }
 
     @RequestMapping("/")
     public String home()
