6135bf56c864adc52bdf9777f6576818e74679e4
cwh <805911956qq@gmail.com>
2018-01-10 16:31:55 +0800

    熔断器仪表盘加turbine

---
+++ hystrix-turbine/src/main/java/com/example/cwh/HystrixTurbineApplication.java
@@ -0,0 +1,18 @@
+package com.example.cwh;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 通过@EnableTurbine接口，激活对Turbine的支持。
+ * @author cwh
+ */
+@SpringBootApplication
+@EnableTurbine
+public class HystrixTurbineApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(HystrixTurbineApplication.class).web(true).run(args);
+	}
+}
+
