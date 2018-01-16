1a1bbc336aaa2de082c317d946e6096a4f36f746
cwh <805911956qq@gmail.com>
2018-01-16 16:35:10 +0800

    spring config增加stream bus实现实时更新配置文件

--- config-server/src/main/java/com/example/cwh/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/cwh/ConfigServerApplication.java
@@ -13,10 +13,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication
-{
-    public static void main(String[] args)
-    {
+public class ConfigServerApplication {
+    public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
