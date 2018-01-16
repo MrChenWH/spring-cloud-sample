8f278e5bbe6cff3947c40959a6a4e793abfc2dab
cwh <805911956qq@gmail.com>
2018-01-16 13:39:31 +0800

    负载均衡测试模块

---
+++ producer2/src/main/java/com/example/cwh/Producer2Application.java
@@ -0,0 +1,17 @@
+package com.example.cwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author cwh
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Producer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Producer2Application.class, args);
+    }
+}
