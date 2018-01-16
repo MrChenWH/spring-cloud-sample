5e1081b902067aee1db9fa44756ba1f34e9fd527
cwh <805911956qq@gmail.com>
2018-01-16 11:12:22 +0800

    负载均衡测试模块

---
+++ producer/src/main/java/com/example/cwh/ProducerApplication.java
@@ -0,0 +1,17 @@
+package com.example.cwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author cwh
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+}
---
+++ producer/src/main/java/com/example/cwh/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.cwh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cwh
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is two messge";
+    }
+}
